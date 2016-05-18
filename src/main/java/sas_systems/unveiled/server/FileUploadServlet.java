4467ac1ddc0a709e4d08d0006ffca7d848dea3b8
Schmidl <D062321@global.corp.sap>
2016-05-18 13:10:41 +0200

    changed fileupload to use multipart

--- src/main/java/sas_systems/unveiled/server/FileUploadServlet.java
+++ src/main/java/sas_systems/unveiled/server/FileUploadServlet.java
@@ -21,13 +21,23 @@ import java.util.Date;
 import java.util.Properties;
 
 import javax.servlet.ServletException;
+import javax.servlet.annotation.MultipartConfig;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.Part;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+// folder for temp files, threshold for storing files on disk (1MB), max file size (5GB)
+@MultipartConfig(fileSizeThreshold=1024*1024, maxFileSize=1024*1024*1024*5)
 public class FileUploadServlet extends HttpServlet {
 
 	private static final long serialVersionUID = -6308606465526504820L;
+	private static final Logger LOG = LoggerFactory.getLogger(FileUploadServlet.class);
+	
+	public static final String TMP_DIR = System.getProperty("java.io.tmpdir");
 	
 	private final String mediaFolder;
 	private final String urlMediaPathPrefix;
@@ -42,26 +52,25 @@ public class FileUploadServlet extends HttpServlet {
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// get file content
+		final Part filePart = request.getPart("file");
+		if(filePart == null) {
+			response.sendError(HttpServletResponse.SC_BAD_REQUEST, "no file found in request (use: parameter 'file' for file content)");
+			return;
+		}
 		// read parameters:
-		String filename = "undefined";
-		String suffix = "unv";
+		String filePartName = "undefined.unv";
 		int author = -1;
-		String mediatype = "";
 		double lat = .0;
 		double lng = .0;
 		boolean isPublic = false;
-		boolean isVerified = false;
-		if(request.getParameter("filename") != null)
-			filename = request.getParameter("filename");
-		if(request.getParameter("suffix") != null)
-			suffix = request.getParameter("suffix");
+		if(getFileName(filePart) != null)
+			filePartName = getFileName(filePart);
 		if(request.getParameter("author") != null) {
 			try {
 				author = Integer.valueOf(request.getParameter("author"));
 			} catch(NumberFormatException e) {};
 		}
-		if(request.getParameter("mediatype") != null)
-			mediatype = request.getParameter("mediatype");
 		if(request.getParameter("latitude") != null) {
 			try {
 				lat = Double.valueOf(request.getParameter("latitude"));
@@ -74,16 +83,16 @@ public class FileUploadServlet extends HttpServlet {
 		}
 		if(request.getParameter("public") != null)
 			isPublic = Boolean.parseBoolean(request.getParameter("public"));
-		if(request.getParameter("verified") != null)
-			isVerified = Boolean.parseBoolean(request.getParameter("verified"));
 		
 		// create and write to file
+		final String filename = filePartName.substring(0, filePartName.indexOf('.'));
+		final String suffix = filePartName.substring(filePartName.indexOf('.')+1, filePartName.length());
 		final long startTime = System.nanoTime();
 		final String location = this.mediaFolder + String.valueOf(author) + "/";
 		final FileWriter writer = new FileWriter(location, filename, suffix);
 		File fileHandle;
 		try {
-			writer.writeToFile(request.getInputStream());
+			writer.writeToFile(filePart.getInputStream());
 			fileHandle = writer.close();
 		} catch(IOException e) {
 			response.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR, e.toString());
@@ -94,16 +103,20 @@ public class FileUploadServlet extends HttpServlet {
 		final String caption = fileHandle.getName();
 		final String fileUrl = this.urlMediaPathPrefix + String.valueOf(author) + "/" + fileHandle.getName();
 		final String thumbnailUrl = ""; // FIXME generate thumbnail
+		final String mediatype = filePart.getContentType();
 		final int length = 0;			// FIXME calculate length [in seconds]
 		final int height = 0;			// FIXME calculate resolution [height]x[width]
 		final int width = 0;
 		final String resolution = height + "x" + width; 
 		FilePOJO fileEntity = new FilePOJO(author, caption, filename, fileUrl, thumbnailUrl, mediatype, 
-				new Date(), fileHandle.length(), lat, lng, isPublic, isVerified, length, height, width, resolution);
+				new Date(), fileHandle.length(), lat, lng, isPublic, false, length, height, width, resolution);
 		// TODO: should be a "global" member to not be created on every request
 		final DatabaseConnector database = new DatabaseConnector();
 		final boolean wasInserted = database.insertFile(fileEntity);
+		
+		// release resources
 		database.close();
+		filePart.delete();
 				
 		// send result
 		final long endTime = System.nanoTime();
@@ -111,5 +124,17 @@ public class FileUploadServlet extends HttpServlet {
 		final double elapsedTimeS = elapsedTimeNs/(1e9);
 		response.getWriter().println(filename + "." + suffix + " from " + author + " was succefully uploaded in " + elapsedTimeS + " seconds!");
 		response.getWriter().println("Status of the database: " + wasInserted + " (was inserted)");
+	}
+	
+	private String getFileName(final Part part) {
+	    final String partHeader = part.getHeader("content-disposition");
+	    LOG.info("Part Header = {}", partHeader);
+	    for (String content : part.getHeader("content-disposition").split(";")) {
+	        if (content.trim().startsWith("filename")) {
+	            return content.substring(
+	                    content.indexOf('=') + 1).trim().replace("\"", "");
+	        }
+	    }
+	    return null;
 	}
 }
